7d9a9a388d6c5278ea87777e1f9c3401d133bf3e
nhat-cready <nhatnm@cready.vn>
2023-01-03 11:45:27 +0700

    Fix background color button

--- src/components/base/button/Button.tsx
+++ src/components/base/button/Button.tsx
@@ -11,7 +11,7 @@ function Button(props: IButtonProps) {
   const { children, fullWidth = false, customClassName, onClick } = props;
 
   const buttonClassName = classNames(
-    "inline-flex px-6 py-3 bg-button-primary-color text-white text-center duration-150 bg-indigo-600 rounded-default hover:bg-indigo-700 active:shadow-xl cursor-pointer shadow-[inset_0px_-2px_0px_rgba(0,0,0,0.25)] ",
+    "inline-flex px-6 py-3 bg-primary-color text-white text-center duration-150 rounded-default hover:bg-indigo-700 active:shadow-xl cursor-pointer shadow-[inset_0px_-2px_0px_rgba(0,0,0,0.25)] ",
     {
       "w-full": fullWidth,
     },
@@ -21,7 +21,6 @@ function Button(props: IButtonProps) {
   return (
     <div className={buttonClassName} onClick={onClick}>
       {children}
-      <div className="inline-flex px-6 py-3 bg-primary-color text-white text-center duration-150 rounded-default hover:bg-indigo-700 active:shadow-xl cursor-pointer shadow-[inset_0px_-2px_0px_rgba(0,0,0,0.25)]"></div>
     </div>
   );
 }
